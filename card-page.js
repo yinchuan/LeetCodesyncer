81ae21078725d544aa646907797aa33d5e847af1
yinchuan <yinchuan.blue@gmail.com>
2023-12-25 11:14:38 +1100

    add search link when switch between chapter items

--- card-page.js
+++ card-page.js
@@ -1,10 +1,41 @@
 var observer = new MutationObserver((mutations) => {
+  // guard
+  if (!document.URL.startsWith('https://leetcode.com/explore/learn/card/')) {
+    return;
+  }
+
   const titleElement = document.querySelector('div.question-title');
   if (!titleElement) {
     return;
   }
 
   observer.disconnect();
+
+  addSearchLink(titleElement);
+
+  // run observer when switching between items in a chpater
+  window.addEventListener('pushstate', function () {
+    observer.observe(document.body, {
+      subtree: true,
+      childList: true,
+    });
+  });
+});
+
+observer.observe(document.body, {
+  subtree: true,
+  childList: true,
+});
+
+const addSearchLink = (titleElement) => {
+  // avoid add link again
+  if (
+    Array.from(titleElement.querySelectorAll('a')).some(
+      (e) => e.text == 'Search Question'
+    )
+  ) {
+    return;
+  }
 
   let search = titleElement.textContent.trim().replaceAll(' ', '+');
   let searchURL = `https://leetcode.com/problemset/?search=${search}&page=1`;
@@ -15,9 +46,4 @@ var observer = new MutationObserver((mutations) => {
   link.target = '_blank';
   link.rel = 'noopener noreferrer'; // suggested by Bard for security
   titleElement.appendChild(link);
-});
-
-observer.observe(document.body, {
-  subtree: true,
-  childList: true,
-});
+};
